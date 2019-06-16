2dd4974f483f52bb42312df6499c92c8bf3f2583
g-flex <43682547+g-flex@users.noreply.github.com>
2019-06-16 05:11:59 +0200

    Added chart

--- html/main.js
+++ html/main.js
@@ -1,4 +1,63 @@
 const $ = (selector) => document.querySelector(selector);
+google.charts.load('current', {packages: ['corechart', 'line']});
+
+function drawLineStyles() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'expenseDate');
+      data.addColumn('number', 'Balance');
+	  data.addColumn({type: 'string', role: 'tooltip'});
+	  data.addColumn({type: 'string', role: 'style'});
+
+    //   data.addRows([
+	// 	[new Date("January 31 1980"),5, 'income 5'],[new Date("January 20 1995"),10, 'income 5'], [new Date("March 10 2012"),40, 'income 30'], [new Date(),400, 'income 360']
+	//   ]);
+
+	data.addRows(valuesArray);
+	  
+      var options = {
+	    //dataOpacity: 0.7,
+		legend: {position: 'none'},
+		backgroundColor: {
+			fill:'#6473c9'
+		},
+		hAxis: {
+			titleTextStyle : {color: '#ffffff'},
+			textStyle : {color: '#ffffff'},
+			title: 'Date',
+			format: 'MMM dd',
+			ticks: datesArray,
+			gridlines: {
+				color: 'none',
+				count: 0
+			}
+		},
+        vAxis: {
+		  titleTextStyle : {color: '#ffffff'},
+		  textStyle : {color: '#ffffff'},
+          title: 'Balance',
+		  ticks: balancesArray,
+		  gridlines: {
+				color: '#303f9f',
+				count: 1
+			}
+		},
+		animation: {
+			startup: true,
+			duration: 1500,
+			easing: 'out'
+		},
+        colors: ['#ffffff'],
+        series: {
+          0: {
+            lineWidth: 2
+          }
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	  chart.draw(data, options);
+	}
+
 let fetchedExpenses;
 let balance = new Number();
 let newDateGMT = new Date();
@@ -57,25 +116,46 @@ async function getExpenses(username){
 	});
 }
 
+var valuesArray=[], balancesArray=[0], datesArray=[];
+var pointStyle = 'point {shape-type: circle; size: 8; stroke-width: 3; visible: true; fill-color: #303f9f; stroke-color: #ffffff;}';
+
 function listExpenses(exps){
 	balance = 0;
+	valuesArray=[];
+	balancesArray=[0];
+	datesArray=[];
 	exps.forEach( exp => {
+		let thisValuesArray=[];
 		exp.exp_val = new Number(exp.exp_val);
 		balance = balance - exp.exp_val;
+		balancesArray.push(balance);
 		exp.exp_date = new Date(exp.exp_date);
 		exp.exp_date = new Date(exp.exp_date.valueOf() - GMTDiff);
 		exp.exp_date.setSeconds(0,0);
+		thisValuesArray.push(exp.exp_date);
+		datesArray.push(exp.exp_date);
+		thisValuesArray.push(balance);
 		let newExp = document.createElement('li');
 		if(exp.exp_val < 0){
 			newExp.classList.add('income');
 			exp.exp_val = new Number(-exp.exp_val);
-		};
+			thisValuesArray.push(exp.exp_tag+' +€'+exp.exp_val);
+		} else {
+			thisValuesArray.push(exp.exp_tag+' -€'+exp.exp_val);
+		}
+		thisValuesArray.push(pointStyle);
+		valuesArray.push(thisValuesArray);
 		newExp.dataset.expId = exp.exp_id;
 		newExp.innerHTML = '€<input type="number" value="'+exp.exp_val+'" placeholder="'+exp.exp_val+'"/> - <input type="text" value="'+exp.exp_tag+'" placeholder="'+exp.exp_tag+'"/> - <input type="datetime-local" value="'+exp.exp_date.toISOString().split(".")[0]+'"/>';
 		$('ul').insertBefore(newExp, $('ul').firstChild);
 	});
 	$('.balance>code').innerHTML = ' € '+balance;
+	//console.log('valuesArray: ',valuesArray, '; balancesArray: ',balancesArray);
 }
+
+$('.chart-button').addEventListener('click', function(){
+	google.charts.setOnLoadCallback(drawLineStyles);
+});
 
 function changeListener(){
 	document.querySelectorAll('input').forEach(expInput => {
